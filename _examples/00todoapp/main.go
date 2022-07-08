fa5de6241630f52f312faa6e86b86f7f11e778ac
podhmo <ababjam61+github@gmail.com>
2022-07-09 00:49:15 +0900

    chore: update examples

--- _examples/00todoapp/main.go
+++ _examples/00todoapp/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"reflect"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/render"
+	"github.com/podhmo/quickapi"
 )
 
 type Todo struct {
@@ -18,19 +16,23 @@ type Todo struct {
 	Done  bool   `json:"bool"`
 }
 
-var todos []Todo = []Todo{
-	{Title: "hello", Done: false},
-	{Title: "boo", Done: true},
-	{Title: "byebye", Done: false},
-}
+var todos []Todo
+
+// func init() {
+// 	todos = []Todo{
+// 		{Title: "hello", Done: false},
+// 		{Title: "boo", Done: true},
+// 		{Title: "byebye", Done: false},
+// 	}
+// }
 
 func ListTodo(
 	ctx context.Context,
-	input Empty,
+	input quickapi.Empty,
 ) (output struct {
 	Items []Todo `json:"items"`
 }, err error) {
-	items := make([]Todo, 0, len(todos))
+	var items []Todo
 	for _, x := range todos {
 		if x.Done {
 			continue
@@ -50,67 +52,11 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/todos", Lift(ListTodo))
+	r.Get("/todos", quickapi.Lift(ListTodo))
 
 	port := 8080
 	log.Printf("[Info]  listening: :%d", port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), r); err != nil {
 		log.Printf("[Error] !! %+v", err)
 	}
-}
-
-// ----------------------------------------
-
-type Action[I any, O any] func(ctx context.Context, input I) (output O, err error)
-
-type Empty struct{}
-
-type ErrorResponse struct {
-	Error string `json:"error"`
-	Code  int    `json:"Code"`
-}
-
-var (
-	Pretty bool
-)
-
-func Lift[I any, O any](action Action[I, O]) http.HandlerFunc {
-	var iz I
-	isEmpty := reflect.TypeOf(iz).NumField() == 0
-
-	return func(w http.ResponseWriter, req *http.Request) {
-		var input I
-		if !isEmpty {
-			if err := json.NewDecoder(req.Body).Decode(&input); err != nil {
-				log.Printf("[ERROR] unexpected error (json.Decode): %+v", err) // TODO: structured logging
-				writeJSONError(w, req, err, 500)
-				return
-			}
-		}
-
-		code := 500
-		output, err := action(req.Context(), input)
-		if err != nil {
-			// TODO: handling status code
-			if err := json.NewDecoder(req.Body).Decode(&input); err != nil {
-				if code == 500 {
-					log.Printf("[ERROR] unexpected error: %+v", err) // TODO: structured logging
-				}
-				writeJSONError(w, req, err, code)
-			}
-		}
-
-		ctx := context.WithValue(req.Context(), render.ContentTypeCtxKey, code)
-		render.JSON(w, req.WithContext(ctx), output)
-	}
-}
-
-func writeJSONError(w http.ResponseWriter, req *http.Request, err error, code int) {
-	v := ErrorResponse{Error: "internal server error", Code: code}
-	if code != 500 {
-		v.Error = err.Error()
-	}
-
-	ctx := context.WithValue(req.Context(), render.ContentTypeCtxKey, code)
-	render.JSON(w, req.WithContext(ctx), v)
 }
