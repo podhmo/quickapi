d6bdc1148a980877ddb38e3f8beb4ae9bd883039
podhmo <ababjam61+github@gmail.com>
2022-07-09 12:31:30 +0900

    feat: support query string (prototype) refs #8

--- qbind/bind.go
+++ qbind/bind.go
@@ -6,36 +6,50 @@ import (
 	"log"
 	"net/http"
 	"reflect"
-	"sync"
 
 	"github.com/gorilla/schema"
 )
 
 var (
-	// for parameters binding
-	mu      sync.Mutex
-	decoder = schema.NewDecoder()
+	queryDecoder  = schema.NewDecoder()
+	headerDecoder = schema.NewDecoder()
 )
+
+func init() {
+	queryDecoder.SetAliasTag("query")
+	headerDecoder.SetAliasTag("header")
+}
 
 func Bind[I any](req *http.Request, metadata Metadata) (I, error) {
 	var input I
-	if !metadata.IsEmpty {
+	if metadata.HasData {
 		if err := json.NewDecoder(req.Body).Decode(&input); err != nil {
 			log.Printf("[ERROR] unexpected error (json.Decode): %+v", err) // TODO: structured logging
 			return input, err
 		}
+		// TODO: validation
+	}
+
+	// TODO: handling metadata (query tag)
+	if err := queryDecoder.Decode(&input, req.URL.Query()); err != nil {
+		log.Printf("[DEBUG] unexpected query string: %+v", err)
+	}
+
+	// TODO: handling metadata (header tag)
+	if err := queryDecoder.Decode(&input, req.Header); err != nil {
+		log.Printf("[DEBUG] unexpected query string: %+v", err)
 	}
 	return input, nil
 }
 
 type Metadata struct {
-	IsEmpty bool // Action is empty
+	HasData bool // Action is empty
 }
 
 func Scan[I any, O any](action func(context.Context, I) (O, error)) Metadata {
 	var iz I
 	isEmpty := reflect.TypeOf(iz).NumField() == 0
 	return Metadata{
-		IsEmpty: isEmpty,
+		HasData: !isEmpty,
 	}
 }
