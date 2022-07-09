1423310993c39c4936b176405025a0d56d0c3482
podhmo <ababjam61+github@gmail.com>
2022-07-09 12:50:50 +0900

    test: add test for qbind.Bind refs #8

--- qbind/bind.go
+++ qbind/bind.go
@@ -3,9 +3,12 @@ package qbind
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"reflect"
+	"strconv"
 
 	"github.com/gorilla/schema"
 )
@@ -13,18 +16,24 @@ import (
 var (
 	queryDecoder  = schema.NewDecoder()
 	headerDecoder = schema.NewDecoder()
+	DEBUG         = false
 )
 
 func init() {
 	queryDecoder.SetAliasTag("query")
 	headerDecoder.SetAliasTag("header")
+	if ok, _ := strconv.ParseBool(os.Getenv("DEBUG")); ok {
+		fmt.Println("ok")
+		DEBUG = ok
+	}
+
 }
 
 func Bind[I any](req *http.Request, metadata Metadata) (I, error) {
 	var input I
 	if metadata.HasData {
 		if err := json.NewDecoder(req.Body).Decode(&input); err != nil {
-			log.Printf("[ERROR] unexpected error (json.Decode): %+v", err) // TODO: structured logging
+			log.Printf("[ERROR] unexpected error (json.Decode): %+v, on %T", err, input) // TODO: structured logging
 			return input, err
 		}
 		// TODO: validation
@@ -32,24 +41,48 @@ func Bind[I any](req *http.Request, metadata Metadata) (I, error) {
 
 	// TODO: handling metadata (query tag)
 	if err := queryDecoder.Decode(&input, req.URL.Query()); err != nil {
-		log.Printf("[DEBUG] unexpected query string: %+v", err)
+		if DEBUG {
+			log.Printf("[DEBUG] unexpected query string: %+v, on %T", err, input)
+		}
 	}
 
 	// TODO: handling metadata (header tag)
 	if err := queryDecoder.Decode(&input, req.Header); err != nil {
-		log.Printf("[DEBUG] unexpected query string: %+v", err)
+		if DEBUG {
+			log.Printf("[DEBUG] unexpected header: %+v, on %T", err, input)
+		}
 	}
 	return input, nil
 }
 
 type Metadata struct {
-	HasData bool // Action is empty
+	HasData bool     // Action is empty
+	Queries []string // query string keys (recursive structure is not supported, also embedded)
+	Headers []string // header keys (recursive structure is not supported, also embedded)
 }
 
 func Scan[I any, O any](action func(context.Context, I) (O, error)) Metadata {
 	var iz I
-	isEmpty := reflect.TypeOf(iz).NumField() == 0
+	rt := reflect.TypeOf(iz)
+	if rt.Kind() == reflect.Ptr {
+		rt = rt.Elem()
+	}
+
+	var queries []string
+	var headers []string
+	for i, n := 0, rt.NumField(); i < n; i++ {
+		if v, ok := rt.Field(i).Tag.Lookup("query"); ok {
+			queries = append(queries, v)
+			continue
+		}
+		if v, ok := rt.Field(i).Tag.Lookup("header"); ok {
+			headers = append(headers, v)
+			continue
+		}
+	}
 	return Metadata{
-		HasData: !isEmpty,
+		HasData: rt.NumField()-len(queries)-len(headers) == 0,
+		Queries: queries,
+		Headers: headers,
 	}
 }
---
+++ qbind/bind_test.go
@@ -0,0 +1,30 @@
+package qbind_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/podhmo/or"
+	"github.com/podhmo/quickapi/qbind"
+)
+
+func TestBind(t *testing.T) {
+	type input struct {
+		Sort   string `query:"sort"`
+		Pretty bool   `query:"pretty"`
+
+		Ignored string
+	}
+
+	metadata := qbind.Scan(func(context.Context, input) (interface{}, error) { return nil, nil })
+	req := or.Fatal(http.NewRequest("GET", "/?pretty=true&x=y&sort=-id", nil))(t)
+
+	got := or.Fatal(qbind.Bind[input](req, metadata))(t)
+	want := input{Sort: "-id", Pretty: true}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("GetContext() mismatch (-want +got):\n%s", diff)
+	}
+}
