03a91748825b28cd7245edfda46f84d0e98cf100
podhmo <ababjam61@gmail.com>
2022-07-12 22:34:36 +0900

    Quickapitest (#12)
    * feat: DecodeResponse()
    
    * test: add test
    
    * feat: add DoRequest
    
    * fix: path argument is always needed
    
    * refactor: adjust
    
    * docs: add tests in examples

---
+++ quickapitest/api.go
@@ -0,0 +1,55 @@
+package quickapitest
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// DecodeResponse decodes json response
+func DecodeResponse[T any](
+	t *testing.T,
+	path string,
+	res *http.Response,
+	code int,
+) T {
+	t.Helper()
+
+	defer res.Body.Close()
+	if wantCode, gotCode := code, res.StatusCode; wantCode != gotCode {
+		buf := new(strings.Builder)
+		io.Copy(buf, res.Body)
+		defer t.Logf("\tresponse: %s", buf.String())
+		t.Fatalf("%q, status: want=%d != got=%d", path, wantCode, gotCode)
+	}
+
+	var got T
+	if err := json.NewDecoder(res.Body).Decode(&got); err != nil {
+		t.Errorf("%q, unexpected error (decode %T): %+v", path, got, err)
+	}
+	return got
+}
+
+// DoRequest requests and decode response
+func DoRequest[T any](
+	t *testing.T,
+	req *http.Request,
+	code int,
+	handler http.Handler,
+	options ...func(testing.TB, *http.Response),
+) T {
+	t.Helper()
+
+	rec := httptest.NewRecorder()
+	handler.ServeHTTP(rec, req)
+	res := rec.Result()
+
+	got := DecodeResponse[T](t, req.URL.Path, res, http.StatusOK)
+	for _, opt := range options {
+		opt(t, res)
+	}
+	return got
+}
---
+++ quickapitest/api_test.go
@@ -0,0 +1,52 @@
+package quickapitest
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func TestDecodeResponse(t *testing.T) {
+	want := person{Name: "foo", Age: 20}
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if err := json.NewEncoder(w).Encode(want); err != nil {
+			t.Fatalf("unexpected error (json.Encode): %+v", err)
+		}
+	})
+
+	path := "/users/1"
+	req := httptest.NewRequest("GET", path, nil)
+	rec := httptest.NewRecorder()
+	handler.ServeHTTP(rec, req)
+
+	got := DecodeResponse[person](t, path, rec.Result(), http.StatusOK)
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("DecodeResponse(), path=%q mismatch (-want +got):\n%s", path, diff)
+	}
+}
+
+func TestDoRequest(t *testing.T) {
+	want := person{Name: "foo", Age: 20}
+
+	path := "/users/1"
+	req := httptest.NewRequest("GET", path, nil)
+	got := DoRequest[person](
+		t, req, http.StatusOK,
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			if err := json.NewEncoder(w).Encode(want); err != nil {
+				t.Fatalf("unexpected error (json.Encode): %+v", err)
+			}
+		}))
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("DoRequest(), path=%q mismatch (-want +got):\n%s", path, diff)
+	}
+}
