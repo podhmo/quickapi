52c198b86f424d6a80257975921e9bb5146a1d2d
podhmo <ababjam61+github@gmail.com>
2022-07-13 00:03:22 +0900

    feat: support map

--- qdump/fill.go
+++ qdump/fill.go
@@ -10,36 +10,60 @@ import (
 func Fill[O any](ob O) O {
 	switch rv := reflect.ValueOf(ob); rv.Kind() {
 	case reflect.Slice, reflect.Map:
-		return fill(rv).Interface().(O)
+		if sv, changed := fill(rv); changed {
+			return sv.Interface().(O)
+		}
+		return ob
 	default:
 		log.Printf("unsupported kind=%s, value=%v", rv.Kind(), rv)
 		return ob
 	}
 }
 
-func fill(rv reflect.Value) reflect.Value {
+func fill(rv reflect.Value) (ret reflect.Value, changed bool) {
 	switch rv.Kind() {
 	case reflect.Slice:
 		if rv.IsNil() {
-			return reflect.MakeSlice(rv.Type(), 0, 0)
+			return reflect.MakeSlice(rv.Type(), 0, 0), true
 		}
+
 		// slice,map,struct
 		st := rv.Type().Elem()
 		switch st.Kind() {
-		case reflect.Slice:
+		case reflect.Slice, reflect.Map:
 			for i, n := 0, rv.Len(); i < n; i++ {
 				rf := rv.Index(i)
-				rf.Set(fill(rf))
+				sv, subchanged := fill(rf)
+				if subchanged {
+					changed = true
+					rf.Set(sv)
+				}
 			}
 		}
-		return rv
+		return rv, changed
 	case reflect.Map:
 		if rv.IsNil() {
-			return reflect.MakeMap(rv.Type())
+			return reflect.MakeMap(rv.Type()), true
 		}
-		return rv
+
+		// slice,map,struct
+		st := rv.Type().Elem()
+		switch st.Kind() {
+		case reflect.Slice, reflect.Map:
+			iter := rv.MapRange()
+			for iter.Next() {
+				// skip key (because: JSON's notation)
+				rf := iter.Value()
+				sv, subchanged := fill(rf)
+				if subchanged {
+					changed = true
+					rv.SetMapIndex(iter.Key(), sv)
+				}
+			}
+		}
+		return rv, changed
 	default:
 		log.Printf("unsupported kind=%s, value=%v ...", rv.Kind(), rv)
-		return rv
+		return rv, false
 	}
 }
--- qdump/fill_test.go
+++ qdump/fill_test.go
@@ -16,7 +16,7 @@ func TestFill_Slice(t *testing.T) {
 
 	t.Run("empty", func(t *testing.T) {
 		want := []int{}
-		got := Fill([]int{})
+		got := Fill(want)
 		if !reflect.DeepEqual(want, got) {
 			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
 		}
@@ -51,6 +51,88 @@ func TestFill_Slice2(t *testing.T) {
 	t.Run("nil-values3", func(t *testing.T) {
 		want := [][][]int{{{1}, {}, {2}}, {}, {{}}}
 		got := Fill([][][]int{{{1}, nil, {2}}, nil, {nil}})
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+
+	t.Run("nil-values3-nil", func(t *testing.T) {
+		want := [][][]int{}
+		got := Fill[[][][]int](nil)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+}
+
+func TestFill_Map(t *testing.T) {
+	t.Run("nil", func(t *testing.T) {
+		want := map[string]int{}
+		got := Fill[map[string]int](nil)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+
+	t.Run("empty", func(t *testing.T) {
+		want := map[string]int{}
+		got := Fill(want)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+
+	t.Run("values", func(t *testing.T) {
+		want := map[string]int{"foo": 0, "bar": 1}
+		got := Fill(want)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+}
+
+func TestFill_Map2(t *testing.T) {
+	t.Run("nil", func(t *testing.T) {
+		want := map[string]map[string]int{}
+		got := Fill[map[string]map[string]int](nil)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+
+	t.Run("nil-values2", func(t *testing.T) {
+		want := map[string]map[string]int{
+			"X": {"foo": 0},
+			"Y": {},
+			"Z": {"foo": 0},
+		}
+		got := Fill(map[string]map[string]int{
+			"X": {"foo": 0},
+			"Y": nil,
+			"Z": {"foo": 0},
+		})
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+
+	t.Run("nil-values3", func(t *testing.T) {
+		want := map[string]map[string]map[string]int{
+			"A": {"X": {"foo": 0}, "Y": {}},
+			"B": {},
+		}
+		got := Fill(map[string]map[string]map[string]int{
+			"A": {"X": {"foo": 0}, "Y": nil},
+			"B": nil,
+		})
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+
+	t.Run("nil-values3-nil", func(t *testing.T) {
+		want := map[string]map[string]map[string]int{}
+		got := Fill[map[string]map[string]map[string]int](nil)
 		if !reflect.DeepEqual(want, got) {
 			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
 		}
