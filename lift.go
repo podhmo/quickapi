b8ed7d08627fb1e975cccf7beac3f5d2e7c9425b
podhmo <ababjam61@gmail.com>
2022-07-12 21:51:28 +0900

    Get request (#11)
    * feat: add GetRequest()
    
    * feat: pack request into context

--- lift.go
+++ lift.go
@@ -24,6 +24,9 @@ type Empty struct{}
 func Lift[I any, O any](action Action[I, O]) http.HandlerFunc {
 	metadata := qbind.Scan(action)
 	return func(w http.ResponseWriter, req *http.Request) {
+		req = req.WithContext(qbind.SetRequest(req.Context(), req)) // for qbind.GetRequest() in action
+
+		// binding request body and query-string and headers to input.
 		input, err := qbind.Bind[I](req, metadata)
 		if err != nil {
 			qdump.DumpError(w, req, err, 400)
@@ -31,6 +34,8 @@ func Lift[I any, O any](action Action[I, O]) http.HandlerFunc {
 		}
 
 		output, err := action(req.Context(), input)
+
+		// dumping result or error as json response
 		qdump.Dump(w, req, output, err)
 	}
 }
---
+++ qbind/context.go
@@ -0,0 +1,20 @@
+package qbind
+
+import (
+	"context"
+	"net/http"
+)
+
+type contextKey string
+
+const (
+	requestKey contextKey = "request"
+)
+
+func GetRequest(ctx context.Context) *http.Request {
+	return ctx.Value(requestKey).(*http.Request)
+}
+
+func SetRequest(ctx context.Context, req *http.Request) context.Context {
+	return context.WithValue(ctx, requestKey, req)
+}
