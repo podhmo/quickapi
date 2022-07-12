3b6ae6df66da56c8f3d5068ca749c9b6e9e8bb0f
podhmo <ababjam61+github@gmail.com>
2022-07-12 23:00:13 +0900

    feat: support slice

--- qdump/fill.go
+++ qdump/fill.go
@@ -1,5 +1,10 @@
 package qdump
 
-func Fill[T any](ob T) T {
+import "reflect"
+
+func Fill[O any](ob O) O {
+	if val := reflect.ValueOf(ob); val.Kind() == reflect.Slice && val.IsNil() {
+		ob = reflect.MakeSlice(val.Type(), 0, 0).Interface().(O)
+	}
 	return ob
 }
--- qdump/fill_test.go
+++ qdump/fill_test.go
@@ -5,10 +5,20 @@ import (
 	"testing"
 )
 
-func TestFill_Nil(t *testing.T) {
+func TestFill_Slice(t *testing.T) {
 	want := []int{}
-	got := Fill[[]int](nil)
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
-	}
+
+	t.Run("nil", func(t *testing.T) {
+		got := Fill[[]int](nil)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
+
+	t.Run("empty", func(t *testing.T) {
+		got := Fill([]int{})
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Fill(), want=%#+v != got=%#+v", want, got)
+		}
+	})
 }
