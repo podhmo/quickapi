79dec1f6306bf45caebb5be5b43994028222da5f
podhmo <ababjam61+github@gmail.com>
2022-07-14 22:10:11 +0900

    test: use quickapitest in lift_test.go

--- lift_test.go
+++ lift_test.go
@@ -2,57 +2,36 @@ package quickapi_test
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http/httptest"
 	"reflect"
-	"strings"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/podhmo/quickapi"
+	"github.com/podhmo/quickapi/quickapitest"
 )
-
-func doRequest[I any, O any](t *testing.T, action quickapi.Action[I, O], wantCode int) O {
-	t.Helper()
-
-	req := httptest.NewRequest("GET", "/", nil)
-	rec := httptest.NewRecorder()
-	handler := quickapi.Lift(action)
-	handler(rec, req)
-
-	gotCode := rec.Result().StatusCode
-	if want, got := wantCode, gotCode; want != got {
-		t.Errorf("status-code, want=%d, but got=%d", want, got)
-	}
-
-	var got O
-	if gotCode == 200 {
-		if err := json.NewDecoder(rec.Result().Body).Decode(&got); err != nil {
-			t.Errorf("unexpected error (decode): %+v", err)
-		}
-	} else {
-		buf := new(strings.Builder)
-		io.Copy(buf, rec.Result().Body)
-		t.Logf("response: %s", buf.String())
-	}
-	return got
-}
 
 func TestLift_OK(t *testing.T) {
 	action := func(context.Context, quickapi.Empty) ([]int, error) { return []int{1, 2, 3}, nil }
-	got := doRequest(t, action, 200)
+	handler := quickapi.Lift(action)
+	req := httptest.NewRequest("GET", "/", nil)
 
-	if want := []int{1, 2, 3}; !reflect.DeepEqual(want, got) {
+	got := quickapitest.DoRequest[[]int](t, req, 200, handler)
+	want := []int{1, 2, 3}
+	if !reflect.DeepEqual(want, got) {
 		t.Errorf("data, want=%#+v, but got=%#+v", want, got)
 	}
 }
 
 func TestLift_OK_NilAsEmptySlice(t *testing.T) {
 	action := func(context.Context, quickapi.Empty) ([]int, error) { return nil, nil }
-	got := doRequest(t, action, 200)
+	handler := quickapi.Lift(action)
+	req := httptest.NewRequest("GET", "/", nil)
 
-	if want := []int{}; !reflect.DeepEqual(want, got) {
+	got := quickapitest.DoRequest[[]int](t, req, 200, handler)
+	want := []int{}
+	if !reflect.DeepEqual(want, got) {
 		t.Errorf("data, want=%#+v, but got=%#+v", want, got)
 	}
 }
@@ -62,5 +41,18 @@ func TestLift_NotFound(t *testing.T) {
 	action := func(context.Context, quickapi.Empty) ([]int, error) {
 		return nil, quickapi.NewAPIError(fmt.Errorf("hmm"), code)
 	}
-	doRequest(t, action, code)
+
+	handler := quickapi.Lift(action)
+	req := httptest.NewRequest("GET", "/", nil)
+
+	type errorResponse struct {
+		Code  int    `json:"code"`
+		Error string `json:"error"`
+	}
+
+	got := quickapitest.DoRequest[errorResponse](t, req, code, handler)
+	want := errorResponse{Code: code, Error: "api-error: hmm"}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("FillNil() mismatch (-want +got):\n%s", diff)
+	}
 }
