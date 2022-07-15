05fcf9d0e5997f1283a5b9713e5418aad1358025
podhmo <ababjam61+github@gmail.com>
2022-07-16 05:23:51 +0900

    chore: update example

--- _examples/10emitdoc/main.go
+++ _examples/10emitdoc/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/podhmo/quickapi"
@@ -51,7 +50,7 @@ func AddPet(context.Context, struct {
 
 // FindPetByID returns a pet based on a single ID
 func FindPetByID(context.Context, struct {
-	ID int64 `query:"id" openapi:"path"` // ID of pet to fetch
+	ID int64 `path:"id" openapi:"path"` // ID of pet to fetch
 }) (
 	output Pet,
 	err error,
@@ -61,7 +60,7 @@ func FindPetByID(context.Context, struct {
 
 // DeletePet deletes a pet by ID
 func DeletePet(context.Context, struct {
-	ID int64 `query:"id" openapi:"path"` // ID of pet to fetch
+	ID int64 `path:"id" openapi:"path"` // ID of pet to fetch
 }) (
 	output quickapi.Empty, // return 204
 	err error,
@@ -81,10 +80,10 @@ func main() {
 	Nam sed condimentum est. Maecenas tempor sagittis sapien, nec rhoncus sem sagittis sit amet. Aenean at gravida augue, ac iaculis sem. Curabitur odio lorem, ornare eget elementum nec, cursus id lectus. Duis mi turpis, pulvinar ac eros ac, tincidunt varius justo. In hac habitasse platea dictumst. Integer at adipiscing ante, a sagittis ligula. Aenean pharetra tempor ante molestie imperdiet. Vivamus id aliquam diam. Cras quis velit non tortor eleifend sagittis. Praesent at enim pharetra urna volutpat venenatis eget eget mauris. In eleifend fermentum facilisis. Praesent enim enim, gravida ac sodales sed, placerat id erat. Suspendisse lacus dolor, consectetur non augue vel, vehicula interdum libero. Morbi euismod sagittis libero sed lacinia.
 	Sed tempus felis lobortis leo pulvinar rutrum. Nam mattis velit nisl, eu condimentum ligula luctus nec. Phasellus semper velit eget aliquet faucibus. In a mattis elit. Phasellus vel urna viverra, condimentum lorem id, rhoncus nibh. Ut pellentesque posuere elementum. Sed a varius odio. Morbi rhoncus ligula libero, vel eleifend nunc tristique vitae. Fusce et sem dui. Aenean nec scelerisque tortor. Fusce malesuada accumsan magna vel tempus. Quisque mollis felis eu dolor tristique, sit amet auctor felis gravida. Sed libero lorem, molestie sed nisl in, accumsan tempor nisi. Fusce sollicitudin massa ut lacinia mattis. Sed vel eleifend lorem. Pellentesque vitae felis pretium, pulvinar elit eu, euismod sapien.
 	`
-	define.Get(bc, "/pets", FindPets).Description(strings.TrimSpace(longDescription))
-	define.Post(bc, "/pets", AddPet).Description(strings.TrimSpace(`Creates a new pet in the store. Duplicates are allowed`))
-	define.Get(bc, "/pets/{id}", FindPetByID).Description(strings.TrimSpace(`Returns a pet based on a single ID`))
-	define.Delete(bc, "/pets/{id}", DeletePet).Status(204).Description(strings.TrimSpace(`delete a single pet based on the ID supplied`))
+	define.Get(bc, "/pets", FindPets).Description(longDescription)
+	define.Post(bc, "/pets", AddPet).Description(`Creates a new pet in the store. Duplicates are allowed`)
+	define.Get(bc, "/pets/{id}", FindPetByID).Description(`Returns a pet based on a single ID`)
+	define.Delete(bc, "/pets/{id}", DeletePet).Status(204).Description(`delete a single pet based on the ID supplied`)
 
 	ctx := context.Background()
 	if err := define.EmitDoc(ctx, bc); err != nil {
