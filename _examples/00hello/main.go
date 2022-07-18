a2605f32772fb2c65b65b0c2eb129d763066674c
podhmo <ababjam61+github@gmail.com>
2022-07-19 01:09:52 +0900

    chore: update example

--- _examples/00hello/main.go
+++ _examples/00hello/main.go
@@ -9,17 +9,6 @@ import (
 	"github.com/podhmo/quickapi"
 )
 
-func main() {
-	r := quickapi.DefaultRouter()
-	r.Get("/", quickapi.Lift(Hello))
-
-	port := 8080
-	log.Printf("[Info]  listening: :%d", port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), r); err != nil {
-		log.Printf("[Error] !! %+v", err)
-	}
-}
-
 type HelloOutput struct {
 	Hello string `json:"hello"`
 }
@@ -27,4 +16,14 @@ type HelloOutput struct {
 func Hello(ctx context.Context, input quickapi.Empty) (output HelloOutput, err error) {
 	output.Hello = "world"
 	return
+}
+
+func main() {
+	handler := quickapi.Lift(Hello)
+
+	port := 8080
+	log.Printf("[INFO]  listening: :%d", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), handler); err != nil {
+		log.Printf("[ERROR] !! %+v", err)
+	}
 }
