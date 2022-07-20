896ea20cd6c11f7b75e448444617d587d43812f0
podhmo <ababjam61+github@gmail.com>
2022-07-21 00:49:00 +0900

    feat: adding GetLogger() function

--- shared/context.go
+++ shared/context.go
@@ -2,6 +2,7 @@ package shared
 
 import (
 	"context"
+	"log"
 	"net/http"
 )
 
@@ -14,7 +15,25 @@ const (
 func GetRequest(ctx context.Context) *http.Request {
 	return ctx.Value(requestKey).(*http.Request)
 }
-
 func SetRequest(ctx context.Context, req *http.Request) context.Context {
 	return context.WithValue(ctx, requestKey, req)
+}
+
+const (
+	loggerKey contextKey = "logger"
+)
+
+type logger interface {
+	Printf(format string, v ...any)
+}
+
+func GetLogger(ctx context.Context) logger {
+	l := ctx.Value(loggerKey)
+	if l == nil {
+		return log.Default()
+	}
+	return l.(logger)
+}
+func SetLogger(ctx context.Context, l logger) context.Context {
+	return context.WithValue(ctx, loggerKey, l)
 }
