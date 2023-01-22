4b1145301341bc2b312a39c8a182b0c6af93f96a
podhmo <ababjam61@gmail.com>
2023-01-23 00:23:11 +0900

    Update example with fix (#68)
    * fix: use in tag and tagNameOption
    
    * chore: update main.go
    
    * chore: update example
    
    * chore: update example
    
    * chore: use method instead of function
    
    * fix: add x-go-type

--- experimental/define/api.go
+++ experimental/define/api.go
@@ -30,12 +30,19 @@ type BuildContext struct {
 func NewBuildContext(docM DocModifier, r chi.Router, options ...func(c *reflectopenapi.Config)) (*BuildContext, error) {
 	doc := docM()
 	c := &reflectopenapi.Config{
+		TagNameOption: &reflectopenapi.TagNameOption{
+			NameTag:        "json",
+			ParamTypeTag:   "in",
+			DescriptionTag: "description",
+			OverrideTag:    "openapi-override",
+			XNewTypeTag:    "x-go-type",
+		},
 		Doc:          doc,
 		DefaultError: shared.ErrorResponse{},
 		StrictSchema: true,
 		IsRequiredCheckFunction: func(tag reflect.StructTag) bool {
 			required := true
-			if val, ok := tag.Lookup("openapi"); ok && val != "body" {
+			if val, ok := tag.Lookup("in"); ok && val != "body" {
 				required = false
 			}
 			if _, isOptional := tag.Lookup("optional"); isOptional {
