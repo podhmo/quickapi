7059b31e0162a45828dc0d9169ef44d2becb6b3e
podhmo <ababjam61+github@gmail.com>
2023-01-28 22:59:03 +0900

    feat: support /metadata/filepath*

--- normalize_template_path_test.go
+++ normalize_template_path_test.go
@@ -20,6 +20,11 @@ func TestNormalizeTemplatedPath(t *testing.T) {
 		{path: "/foo/{foo_id}/bar/{bar_id}", normalized: "/foo/{foo_id}/bar/{bar_id}", vars: map[string]string{"foo_id": "", "bar_id": ""}},
 		{path: "/foo/{foo_id}/articles/{rid:^[0-9]{5,6}}", normalized: "/foo/{foo_id}/articles/{rid}", vars: map[string]string{"foo_id": "", "rid": "^[0-9]{5,6}"}},
 		{path: "/foo/{id:^(number)?:?[0-9]+}", normalized: "/foo/{id}", vars: map[string]string{"id": "^(number)?:?[0-9]+"}},
+		// RFC6570 foo*
+		// - https://github.com/OAI/OpenAPI-Specification/issues/291
+		// - https://github.com/OAI/OpenAPI-Specification/issues/892
+		{path: "/api/metadata/*", normalized: "/api/metadata/{/STAR*}", vars: map[string]string{"/STAR*": ""}},
+		{path: "/version/{id}/api/metadata/*", normalized: "/version/{id}/api/metadata/{/STAR*}", vars: map[string]string{"id": "", "/STAR*": ""}},
 	}
 	for _, c := range cases {
 		c := c
